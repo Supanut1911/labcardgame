3dc25a547379d2d584e9c7ae83092271c5c0755d
nut <ynotmela@gmail.com>
2018-09-22 17:19:31 +0700

    7

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -10,18 +10,22 @@ export default class CharacterCard extends Component {
         }
     }
 
+
+     
+    
     activate = () => {
-        if (!this.state.active) {
-            this.setState({ active: true})
+        if(!this.state.active){
+        this.props.activationHandler(this.props.value)
+        this.setState({active: true})
         }
-    }
-       
+       }
+
 
 
     render() {
         let className = `card ${this.state.active ? 'activeCard' : ''}`
         return (
-            <div className= {className} onClick = {this.activate}>{this.props.value}
+            <div className= {className} onClick = {this.activate }>{this.props.value}
             
             
             </div>
--- src/WordCard.js
+++ src/WordCard.js
@@ -3,12 +3,28 @@ import logo from './logo.svg';
 
 import CharacterCard from './CharacterCard';
 
-export default class WordCard extends
-    Component {
+
+
+export default class WordCard extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            attemp: '',
+        }
+    }
+
+
+    activationHandler = c => { console.log(`${c} has been activated.`) }
+
+
+
     render() {
         return (
             <div>
-                {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} />)}
+                {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt}
+                activationHandler={this.activationHandler}/>
+                )}
             </div>
         );
     }
